26020047f83ef0d6c8c5d6fc59d062baafab774a
Dmitry <dlisak@devtoday.com>
2018-09-10 11:06:28 +0300

    Sending data to the server

--- ui/src/components/main/forms/input.vue
+++ ui/src/components/main/forms/input.vue
@@ -1,12 +1,23 @@
 <template>
 <div id="input">
-   <input name="username" type="text" class="input username" value="Username" onfocus="this.value=''" />
-   <input name="password" type="password" class="input password" value="Password" onfocus="this.value=''" />
+   <input v-model="inputData.login" name="username" type="text" class="input username" value="Username" />
+   <input v-model="inputData.pass" name="password" type="password" class="input password" value="Password" />
    </div>
 </template>
 
 <script>
-
+  export default {
+    data() { return {
+      inputData: {
+        login : '',
+        pass: '',
+      },
+    }
+    },
+    created(){
+      this.$emit("sendInputData", this.inputData);
+    }
+  }
 </script>
 
 <style>
--- ui/src/system/routes.js
+++ ui/src/system/routes.js
@@ -1,7 +1,9 @@
-
 import main from '../components/main/pages/main.vue'
 
 
 export const routes = [
-  {path: '/', component: main}
-]
+  {
+    path: '/',
+    component: main
+  }
+];
