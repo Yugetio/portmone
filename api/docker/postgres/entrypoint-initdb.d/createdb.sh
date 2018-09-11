8b80c932d9a11a99009767f2319d5899be917dc2
B_Hordii <bhordiy@devtoday.com>
2018-09-11 16:59:51 +0300

    11-09.16:59

--- api/docker/postgres/entrypoint-initdb.d/createdb.sh
+++ api/docker/postgres/entrypoint-initdb.d/createdb.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
- psql -v ON_ERROR_STOP=1 --username "api_user" --dbname "portmone" <<-EOSQL
-     CREATE USER portmone WITH PASSWORD '@GR&G)*(@HR#&RH';
+ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+     CREATE USER portmone WITH PASSWORD 'GR&G)(HR&RH';
      CREATE DATABASE portmone;
      GRANT ALL PRIVILEGES ON DATABASE portmone TO portmone;
  EOSQL
