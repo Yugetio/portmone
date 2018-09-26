ecb8e308239bb0a7aa51b35e34e4033dd4e8acd5
Dmitry <dlisak@devtoday.com>
2018-09-26 14:59:55 +0300

    26.09.2018: Final design

--- ui/src/components/main/elements/description/Description.vue
+++ ui/src/components/main/elements/description/Description.vue
@@ -1,20 +1,8 @@
 <template>
-<div class="wrapper-description">
-
-
-  <div class="item-background">
-    <img src="./../../../../assets/images/desk.jpg" alt="">
-  </div>
-  <div class="text-wrapper">
-    <div  class="text">
-      <h1>Перевести гроші  <br>
-з картки на картку</h1>
-      <p class="secondary-text"> Все просто!</p>
-      <p> Molestias excepturi sint, obcaecati cupiditate non numquam eius modi. <br>Accusantium doloremque laudantium, totam rem aperiam eaque ipsa quae.<br> Hic tenetur a sapiente delectus, ut perspiciatis, unde
-        omnis iste natus error.</p>
-    </div>
-
-  </div>
+<div class="text">
+  <h1>створити електронний гаманець </h1>
+  <p class="secondary-text"> Все просто!</p>
+  <p  class="description-text"> Molestias excepturi sint, obcaecati cupiditate non numquam eius modi. <br>Accusantium doloremque laudantium, totam rem aperiam eaque ipsa quae.<br> Hic tenetur a sapiente delectus, ut perspiciatis, unde omnis iste natus error.</p>
 </div>
 </template>
 
--- ui/src/components/main/header-footer/Footer.vue
+++ ui/src/components/main/header-footer/Footer.vue
@@ -1,15 +1,15 @@
 <template>
-  <footer>
-<div class="footer-security">
-<ul>
-  <li><img src="./../../../assets/images/1.png" alt=""></li>
-  <li><img src="./../../../assets/images/2.png" alt=""></li>
-  <li><img src="./../../../assets/images/3.png" alt=""></li>
-  <li><img src="./../../../assets/images/4.png" alt=""></li>
-  <li><img src="./../../../assets/images/5.png" alt=""></li>
-</ul>
-</div>
-  </footer>
+<footer>
+  <div class="footer-security">
+    <ul>
+      <li><img src="./../../../assets/images/2.png" alt=""></li>
+      <li><img src="./../../../assets/images/3.png" alt=""></li>
+    </ul>
+  </div>
+  <div class="copyright">
+     <p>Copyright &copy; 2018 Dmitry Lysak | Bohdan Hordii</p>
+  </div>
+</footer>
 </template>
 
 <script>
