4d1298b4c4cedf7fb0149f0317f2f5a8ddcc9c63
Dmitry <dlisak@devtoday.com>
2018-09-27 16:54:59 +0300

    27.09.2018: Site store created

--- ui/src/components/main/elements/description/Description.vue
+++ ui/src/components/main/elements/description/Description.vue
@@ -2,7 +2,7 @@
 <div class="text">
   <h1>створити електронний гаманець </h1>
   <p class="secondary-text"> Все просто!</p>
-  <p  class="description-text"> Molestias excepturi sint, obcaecati cupiditate non numquam eius modi. <br>Accusantium doloremque laudantium, totam rem aperiam eaque ipsa quae.<br> Hic tenetur a sapiente delectus, ut perspiciatis, unde omnis iste natus error.</p>
+  <p  class="description-text"> Molestias excepturi sint, obcaecati cupiditate non numquam eius modi. <br>Accusantium doloremque laudantidfgfum, totam rem aperiam eaque ipsa quae.<br> Hic tenetur a sapiente delectus, ut perspiciatis, unde omnis iste natus error.</p>
 </div>
 </template>
 
---
+++ ui/src/store/modules/user.js
@@ -0,0 +1,23 @@
+//користувач нашого сайту. null - пока не авторизувався
+const state = {
+  user: null
+}
+
+const mutations = {
+
+}
+//користувач робить дію (actions) де викликає (mutations) які змінять стан user
+const actions = {
+
+}
+// за допомогою getters отримуєм данні про user
+const getters = {
+
+}
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters
+}
