eeec205068dc94177d434d4d37c40568a8c2d2b1
Artem Oliinyk <aoliynyk@devtoday.com>
2018-11-15 14:57:19 +0200

    added some search to transactionController

--- api/src/Portmone/Controller/TransactionController.php
+++ api/src/Portmone/Controller/TransactionController.php
@@ -11,9 +11,10 @@ use Symfony\Component\Config\Definition\Exception\Exception;
 use App\Repository\TransactionEntityRepository;
 use Elastica\Client;
 use Elastica\Document;
+use Elasticsearch\ClientBuilder;
 
 
-class TransactionController
+class TransactionController extends Controller
 {
 
     /**
@@ -53,6 +54,97 @@ class TransactionController
         } catch (Exception $e) {
             return $this->fail($e);
         }
+    }
+
+    /**
+     * @Route("/money", methods="POST")
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function searchTransactionSameTransferredMoney(Request $request)
+    {
+        $client = $this->getSearchClient();
+        $params = [
+            'index' => 'portmone',
+            'type' => 'transaction',
+            'body' => [
+                'query' => [
+                    'match' => [
+                        'transferredMoney' => $request->get('money')
+                    ],
+                ]
+            ]
+        ];
+        $response = $client->search($params);
+
+        return new JsonResponse($response);
+    }
+
+    /**
+     * @Route("/money_great_than", methods="POST")
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function searchTransactionGreatThanTransferredMoney(Request $request)
+    {
+        $client = $this->getSearchClient();
+        $params = [
+            'index' => 'portmone',
+            'type' => 'transaction',
+            'body' => [
+                'query' => [
+                    'range' => [
+                        'transferredMoney' => [
+                            'gt' => $request->get('money')],
+                    ]
+                ]
+            ]
+        ];
+        $response = $client->search($params);
+
+        return new JsonResponse($response);
+    }
+
+
+    /**
+     * @Route("/date_after", methods="POST")
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function searchTransactionOfDate(Request $request)
+    {
+        // example "date": "2018/11/16", "date": "2018-11-16",
+        $requestDate = new \DateTime($request->get('date'));
+        $requestTimestamp = $requestDate->getTimestamp();
+        $client = $this->getSearchClient();
+        $params = [
+            'index' => 'portmone',
+            'type' => 'transaction',
+            'body' => [
+                'query' => [
+                    'range' => [
+                        'date' =>[
+                            'gt' => $requestTimestamp
+                            ],
+                    ]
+                ]
+            ]
+        ];
+        $response = $client->search($params);
+
+        return new JsonResponse($response);
+    }
+
+
+
+    public function getSearchClient()
+    {
+        $hostName = [
+            $_ENV['ELASTICSEARCH_HOST']
+        ];
+        $client = ClientBuilder::create()->setHosts($hostName)->build();
+
+        return $client;
     }
 
     private function fail(\Exception $e)
