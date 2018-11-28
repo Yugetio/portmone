60cc5fc8c9385d2493902b983ea71465bc81330c
Vladyslav Kaminskyi <vkaminsky@devtoday.com>
2018-11-28 13:06:13 +0200

    added tests for cards getters and mutations

---
+++ ui/src/__tests__/cards/mutations.test.js
@@ -0,0 +1,27 @@
+import store from "../../store/modules/cards"
+const { mutations } = store;
+
+describe("Cards mutation", () => {
+  const state = {
+    cards: []
+  }
+
+  it("show popup", () => {
+    const data = [{
+      id: 1,
+      number: '1111111111111',
+      cash: '121'
+    },
+    {
+      id: 2,
+      number: '22222222222222',
+      cash: '121'
+    }]
+    
+    mutations.SET_CARDS(state, data)
+
+    expect(state).toEqual({
+      cards: data
+    })
+  })
+})
